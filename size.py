f83629f741a69177929ef3c226510f034ddd4406
Christopher Castro <63797614+Christopher-Castro@users.noreply.github.com>
2023-03-23 13:53:49 -0500

    Update size.py

--- size.py
+++ size.py
@@ -1,9 +1,10 @@
 import cv2
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # this is the magic!
-
+cap = cv2.VideoCapture(0)
+# set resolution to 1280x720
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-r, frame = cap.read()
-print('Resolution: ' + str(frame))
+print(width,height)
